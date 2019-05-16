e5c569f3d132785690a1c2d6e2d3649f1e5a52db
github username <github email address>
2019-05-16 18:19:00 -0400

    message

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,26 @@
 class Animal
 
+  @@all = []
+
+  attr_accessor :weight, :zoo
+  attr_reader :species, :nickname
+
+  def initialize(species,weight,nickname,zoo)
+    @species = species
+    @weight = weight
+    @nickname = nickname
+    @zoo = zoo
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_species(species)
+    self.all.select do |animal|
+      animal.species == species
+    end
+  end
+
 end
--- run.rb
+++ run.rb
@@ -3,7 +3,13 @@ require_relative "lib/Zoo.rb"
 require 'pry'
 
 
-#Test your code here
+brooklyn_zoo = Zoo.new("Brooklyn Zoo", "Brooklyn","lion")
+bronx_zoo = Zoo.new("Bronx Zoo","Bronx","king_cobra")
+
+lion = Animal.new("cat",400,"pat",brooklyn_zoo)
+king_cobra = Animal.new("reptile",13,"snape",bronx_zoo)
+
+
 
 
 binding.pry
